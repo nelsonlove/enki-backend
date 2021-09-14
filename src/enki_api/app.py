5da2828c43945f28df5ba5654f27630f427fce81
nelsonlove <nelson.love@me.com>
2021-09-14 15:45:09 -0400

    Created database.py with init_app function; created models.py; added SQLAlchemy config

--- src/enki_api/app.py
+++ src/enki_api/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import database
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+
+database.init_app(app)
 
 
 if __name__ == '__main__':
