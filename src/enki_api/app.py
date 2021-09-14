bb72c2790bcfc1143791f1e005d5124fcf30f616
nelsonlove <nelson.love@me.com>
2021-09-14 16:19:16 -0400

    Added Chat, Prompt, User and base classes to models.py

--- src/enki_api/app.py
+++ src/enki_api/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+app.config['SECRET_KEY'] = 'ultratopsecretkey'
+app.config['DEBUG'] = True
 
 database.init_app(app)
 
