37a3a74059e715ca1631fe803c5efff17b5313f9
nelsonlove <nelson.love@me.com>
2021-09-14 20:31:19 -0400

    Revised abstract base models for use with flask-rest-jsonapi

--- src/enki_api/model.py
+++ src/enki_api/model.py
@@ -1,41 +1,33 @@
 import json
-from datetime import datetime
+
+from sqlalchemy.orm import declared_attr
 
 from .database import db
 
 
 class BaseModel(db.Model):
-    __tablename__ = 'base'
+    __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(50))
 
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.now())
-    date_modified = db.Column(db.DateTime, nullable=True)
+    date_created = db.Column(db.DateTime, default=db.func.now())
+    date_modified = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
     private = db.Column(db.Boolean, default=True)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'base',
-        'polymorphic_on': type
-    }
 
     def __repr__(self):
         return f'<{self.__class__.__name__} #{self.id}>'
 
 
 class Asset(BaseModel):
-    __tablename__ = 'asset'
-
-    id = db.Column(db.Integer, db.ForeignKey('base.id'), primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    __abstract__ = True
 
     name = db.Column(db.Text, nullable=True)
     description = db.Column(db.Text, nullable=True)
     _messages = db.Column(db.Text, nullable=False)
 
-    __mapper_args__ = {
-        'polymorphic_identity': 'asset',
-    }
+    @declared_attr
+    def owner_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('user.id'))
 
     @property
     def messages(self):
@@ -48,50 +40,14 @@ class Asset(BaseModel):
         self._messages = json.dumps(message_list)
 
 
-class User(BaseModel):
-    __tablename__ = 'user'
-
-    id = db.Column(db.Integer, db.ForeignKey('base.id'), primary_key=True)
-
-    username = db.Column(db.String(120), unique=True, nullable=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    api_key = db.Column(db.String(80), unique=True, nullable=False)
-
-    prompts = db.relationship('Prompt', backref=db.backref('owner', lazy=True), foreign_keys=[Asset.owner_id])
-    chats = db.relationship('Chat', backref=db.backref('owner', lazy=True), foreign_keys=[Asset.owner_id])
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'user',
-    }
-
-    def __repr__(self):
-        return f'<User "{self.display_name}">'
-
-    @property
-    def assets_last_active(self):
-        last_chat = sorted(self.chats, key=lambda c: c.date_last_active, reverse=True)[0]
-        last_prompt = sorted(self.prompts, key=lambda p: p.date_last_active, reverse=True)[0]
-        return {
-            'prompt': last_prompt,
-            'chat': last_chat
-        }
-
-
 class Chat(Asset):
-    __tablename__ = 'chat'
-
-    id = db.Column(db.Integer, db.ForeignKey('asset.id'), primary_key=True)
     prompt_id = db.Column(db.Integer, db.ForeignKey('prompt.id'), nullable=False)
 
-    # owner = db.relationship("User", backref=db.backref('chats', lazy=True), foreign_keys=[Asset.owner_id])
-    # prompt = db.relationship("Prompt", backref=db.backref('chats', lazy=True), foreign_keys=[Prompt.id])
+    owner = db.relationship("User", backref=db.backref('chats', lazy=True))
+    prompt = db.relationship("Prompt", backref=db.backref('chats', lazy=True))
 
 
 class Prompt(Asset):
-    __tablename__ = 'prompt'
-
-    id = db.Column(db.Integer, db.ForeignKey('asset.id'), primary_key=True)
-
     # GPT args
     intro_text = db.Column(db.Text, nullable=False)
     bot_name = db.Column(db.String(255), nullable=False)
@@ -101,9 +57,19 @@ class Prompt(Asset):
     frequency_penalty = db.Column(db.Float(2), nullable=False, default=0.0)
     presence_penalty = db.Column(db.Float(2), nullable=False, default=0.0)
 
-    # owner = db.relationship("User", backref=db.backref('prompts', lazy=True), foreign_keys=[Asset.owner_id])
-    chats = db.relationship('Chat', backref=db.backref('prompt', lazy=True), foreign_keys=[Chat.prompt_id])
+    owner = db.relationship('User', backref=db.backref('prompts'))
 
-    __mapper_args__ = {
-        'polymorphic_identity': 'prompt',
-    }
+
+class User(BaseModel):
+    username = db.Column(db.String(120), unique=True, nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    api_key = db.Column(db.String(80), unique=True, nullable=False)
+
+    @property
+    def assets_last_active(self):
+        last_chat = sorted(self.chats, key=lambda c: c.date_last_active, reverse=True)[0]
+        last_prompt = sorted(self.prompts, key=lambda p: p.date_last_active, reverse=True)[0]
+        return {
+            'prompt': last_prompt,
+            'chat': last_chat
+        }
