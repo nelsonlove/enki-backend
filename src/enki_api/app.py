6e281b68e4f7f0a23f74d6d65f852013096fe32b
nelsonlove <nelson.love@me.com>
2021-09-14 20:32:40 -0400

    Added prompt endpoints

--- src/enki_api/app.py
+++ src/enki_api/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_rest_jsonapi import Api
 
-from .resource import UserList, UserDetail
 from . import database
+from .resource import UserList, UserDetail, PromptList, PromptDetail, UserRelationship, PromptRelationship
 
 app = Flask(__name__)
 
@@ -14,9 +14,13 @@ database.init_app(app)
 db = database.db
 
 api = Api(app)
-api.route(UserList, 'user_list', '/users')
-api.route(UserDetail, 'user_detail', '/users/<int:id>')
 
+api.route(UserList, 'user_list', '/users')
+api.route(UserDetail, 'user_detail', '/users/<int:id>', '/prompts/<int:prompt_id>/owner')
+api.route(UserRelationship, 'user_prompts', '/users/<int:id>/relationships/prompts')
+api.route(PromptList, 'prompt_list', '/prompts', '/users/<int:id>/prompts')
+api.route(PromptDetail, 'prompt_detail', '/prompts/<int:id>')
+api.route(PromptRelationship, 'prompt_user', '/prompts/<int:id>/relationships/owner')
 
 if __name__ == '__main__':
     app.run()
--- src/enki_api/resource.py
+++ src/enki_api/resource.py
@@ -1,8 +1,10 @@
-from flask_rest_jsonapi import ResourceList, ResourceDetail
+from flask_rest_jsonapi import ResourceList, ResourceDetail, ResourceRelationship
+from flask_rest_jsonapi.exceptions import ObjectNotFound
+from sqlalchemy.exc import NoResultFound
 
 from src.enki_api.database import db
-from src.enki_api.model import User
-from src.enki_api.schema import UserSchema
+from src.enki_api.model import User, Prompt  # , Chat
+from src.enki_api.schema import UserSchema, PromptSchema
 
 
 class UserList(ResourceList):
@@ -14,8 +16,70 @@ class UserList(ResourceList):
 
 
 class UserDetail(ResourceDetail):
+    def before_get_object(self, view_kwargs):
+        if view_kwargs.get('prompt_id') is not None:
+            try:
+                prompt = self.session.query(Prompt).filter_by(id=view_kwargs['prompt_id']).one()
+            except NoResultFound:
+                raise ObjectNotFound({'parameter': 'prompt_id'},
+                                     "Prompt: {} not found".format(view_kwargs['prompt_id']))
+            else:
+                if prompt.owner is not None:
+                    view_kwargs['id'] = prompt.owner.id
+                else:
+                    view_kwargs['id'] = None
+
     schema = UserSchema
     data_layer = {
         'session': db.session,
         'model': User,
+        'methods': {
+            'before_get_object': before_get_object,
+        }
     }
+
+
+class UserRelationship(ResourceRelationship):
+    schema = UserSchema
+    data_layer = {'session': db.session,
+                  'model': User}
+
+
+class PromptList(ResourceList):
+    def query(self, view_kwargs):
+        query_ = self.session.query(Prompt)
+        if view_kwargs.get('id') is not None:
+            try:
+                self.session.query(User).filter_by(id=view_kwargs['id']).one()
+            except NoResultFound:
+                raise ObjectNotFound({'parameter': 'id'}, "User: {} not found".format(view_kwargs['id']))
+            else:
+                query_ = query_.join(User).filter(User.id == view_kwargs['id'])
+        return query_
+
+    def before_create_object(self, data, view_kwargs):
+        if view_kwargs.get('id') is not None:
+            user = self.session.query(User).filter_by(id=view_kwargs['id']).one()
+            data['user_id'] = user.id
+
+    schema = PromptSchema
+    data_layer = {
+        'session': db.session,
+        'model': Prompt,
+        'methods': {
+            'query': query,
+            'before_create_object': before_create_object,
+        }
+    }
+
+
+class PromptDetail(ResourceDetail):
+    schema = PromptSchema
+    data_layer = {'session': db.session,
+                  'model': Prompt}
+
+
+class PromptRelationship(ResourceRelationship):
+    schema = PromptSchema
+    data_layer = {'session': db.session,
+                  'model': Prompt}
