7c725a5231a50b14351ce02921de4e85e46d3c4e
nelsonlove <nelson.love@me.com>
2021-09-17 15:50:19 -0400

    Preliminary POST endpoint at chats/<int:chat_id>/messages

--- src/enki_api/app.py
+++ src/enki_api/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, request
 from flask_rest_jsonapi import Api
 from flask_cors import CORS
 
 from . import database
+from .model import Chat
+from gpt_utils.core import GPT
+from gpt_utils.prompt import ChatPrompt
+
 from .resource import (
     UserList,
     UserDetail,
@@ -49,6 +55,38 @@ api.route(ChatList, 'chat_list', '/chats', '/users/<int:id>/chats')
 api.route(ChatDetail, 'chat_detail', '/chats/<int:id>')
 api.route(ChatRelationship, 'chat_user', '/chats/<int:id>/relationships/owner')
 api.route(ChatRelationship, 'chat_prompt', '/chats/<int:id>/relationships/prompt')
+
+
+@app.route('/chats/<int:chat_id>/messages', methods=['POST'])
+def post_message(chat_id):
+    # print(request.json)
+    # print(request.args)
+    message = request.values.get('message')
+    # print(message)
+    # return message
+    chat = Chat.query.filter_by(id=chat_id)[0]
+    prompt = ChatPrompt(
+        chat.prompt.bot_name,
+        chat.prompt.human_name,
+        *chat.prompt.messages,
+        intro_text=chat.prompt.intro_text
+    )
+    prompt.messages += chat.messages
+    reply = GPT(
+        engine="davinci",
+        stop='\n',
+        max_tokens=chat.prompt.max_tokens,
+        temperature=chat.prompt.temperature,
+        presence_penalty=chat.prompt.presence_penalty,
+        frequency_penalty=chat.prompt.frequency_penalty,
+    ).response(prompt.format(message))
+
+    chat.messages += [[message, reply]]
+    chat.date_updated = datetime.now()
+    db.session.add(chat)
+    db.session.commit()
+    return reply
+
 
 if __name__ == '__main__':
     app.run()
