069815d6abfc7c2a58d3570d95d6cd0c1604f3b9
nelsonlove <nelson.love@me.com>
2021-09-23 12:29:13 -0400

    Tidy formatting in app.py and database.py for clarity

--- src/enki_api/app.py
+++ src/enki_api/app.py
@@ -1,63 +1,21 @@
-import json
-from datetime import datetime
-
-from flask import Flask, request
+from flask import Flask
 from flask_rest_jsonapi import Api
 from flask_cors import CORS
 
-from . import database
-from .model import Chat
-from gpt_utils.core import GPT
-from gpt_utils.prompt import ChatPrompt
-
-from .resource import (
-    UserList,
-    UserDetail,
-    UserRelationship,
-    PromptList,
-    PromptDetail,
-    PromptRelationship,
-    ChatList,
-    ChatDetail,
-    ChatRelationship
-)
+from enki_api import database
+from enki_api import resource
 
 app = Flask(__name__)
-
-CORS(app)
 
 app.config['SECRET_KEY'] = 'ultratopsecretkey'
 app.config['DEBUG'] = True
 
-database.init_app(app)
+CORS(app)
 
 db = database.db
+database.init(app)
 
 api = Api(app)
-api.route(UserList, 'user_list', '/users')
-
-api.route(UserDetail, 'user_detail',
-          '/users/<int:id>',
-          '/prompts/<int:prompt_id>/owner',
-          '/chats/<int:chat_id>/owner'
-          )
-api.route(UserRelationship, 'user_prompts', '/users/<int:id>/relationships/prompts')
-api.route(UserRelationship, 'user_chats', '/users/<int:id>/relationships/chats')
-
-api.route(PromptList, 'prompt_list', '/prompts', '/users/<int:id>/prompts')
-api.route(PromptDetail, 'prompt_detail',
-          '/prompts/<int:id>',
-          '/prompts/<int:prompt_id>',
-          '/chats/<int:chat_id>/prompt'
-          )
-api.route(PromptRelationship, 'prompt_user', '/prompts/<int:id>/relationships/owner')
-api.route(PromptRelationship, 'prompt_chats', '/prompts/<int:id>/relationships/chats')
-
-api.route(ChatList, 'chat_list', '/chats', '/users/<int:id>/chats')
-api.route(ChatDetail, 'chat_detail', '/chats/<int:id>')
-api.route(ChatRelationship, 'chat_user', '/chats/<int:id>/relationships/owner')
-api.route(ChatRelationship, 'chat_prompt', '/chats/<int:id>/relationships/prompt')
-
 
 resource.UserList.add_routes(api)
 resource.UserDetail.add_routes(api)
--- src/enki_api/database.py
+++ src/enki_api/database.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-def init_app(app):
+def init(app):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.test.db'
+
     db.app = app
     db.init_app(app)
     db.create_all(app=app)
