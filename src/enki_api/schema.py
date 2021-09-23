10cadf864643e65ebc1a8c75274baf80e9707d6a
nelsonlove <nelson.love@me.com>
2021-09-23 12:25:14 -0400

    Remove base schema classes; update schemata to match models; add underscore to camelCase inflection for field names

--- src/enki_api/schema.py
+++ src/enki_api/schema.py
@@ -1,33 +1,35 @@
-from datetime import datetime
+import re
 
 from marshmallow_jsonapi import fields
 from marshmallow_jsonapi.flask import Schema, Relationship
 
 
-class BaseSchema(Schema):
-    id = fields.Integer(as_string=True, dump_only=True)
-    date_created = fields.DateTime(dump_only=True)
-    date_modified = fields.DateTime()
-    private = fields.Bool()
+def underscore_to_camel(name):
+    under_pat = re.compile(r'_([a-z])')
+    return under_pat.sub(lambda x: x.group(1).upper(), name)
 
 
-class AssetSchema(BaseSchema):
-    name = fields.Str()
-    description = fields.Str()
-
-
-class UserSchema(BaseSchema):
+class UserSchema(Schema):
     class Meta:
         type_ = 'user'
         self_view = 'user_detail'
         self_view_kwargs = {'id': '<id>'}
         self_view_many = 'user_list'
+        inflect = underscore_to_camel
 
     # TODO move assets_last_active here?
+    id = fields.Integer(as_string=True)
+    date_created = fields.DateTime(dump_only=True)
+    date_modified = fields.DateTime()
+
     auth_id = fields.Str()  # from auth0
     nickname = fields.Str()
     api_key = fields.Str(load_only=True)
-    display_name = fields.Function(lambda obj: obj.nickname or f'Anonymous #{obj.id}')
+
+    visible = fields.Bool()
+    visible_chats = fields.Bool()
+    visible_prompts = fields.Bool()
+
     prompts = Relationship(self_view='user_prompts',
                            self_view_kwargs={'id': '<id>'},
                            related_view='prompt_list',
@@ -35,6 +37,7 @@ class UserSchema(BaseSchema):
                            many=True,
                            schema='PromptSchema',
                            type_='prompt')
+
     chats = Relationship(self_view='user_chats',
                          self_view_kwargs={'id': '<id>'},
                          related_view='chat_list',
@@ -44,31 +47,37 @@ class UserSchema(BaseSchema):
                          type_='chat')
 
 
-class PromptSchema(AssetSchema):
+class PromptSchema(Schema):
     class Meta:
         type_ = 'prompt'
         self_view = 'prompt_detail'
         self_view_kwargs = {'id': '<id>'}
         self_view_many = 'prompt_list'
+        inflect = underscore_to_camel
+
+    id = fields.Integer(as_string=True, dump_only=True)
+    date_created = fields.DateTime(dump_only=True)
+    date_modified = fields.DateTime()
+
+    visible = fields.Bool()
 
     # TODO validation should probably go in here
-    display_name = fields.Function(lambda obj: (obj.name or 'Untitled Prompt #' + str(obj.id)))
-                                                # or obj.description
-                                                # or obj.intro_text[0:max(len(obj.intro_text), 20)]))
-    intro_text = fields.Str()
-    bot_name = fields.Str()
-    human_name = fields.Str()
-    max_tokens = fields.Int()
+
+    intro = fields.Str()
+    bot = fields.Str()
+    human = fields.Str()
+
     temperature = fields.Float()
-    frequency_penalty = fields.Float()
-    presence_penalty = fields.Float()
-    messages = fields.Function(lambda prompt: prompt.messages)
+    variety = fields.Float()
+    verbosity = fields.Int()
+
     owner = Relationship(self_view='prompt_user',
                          self_view_kwargs={'id': '<id>'},
                          related_view='user_detail',
                          related_view_kwargs={'prompt_id': '<id>'},
                          schema='UserSchema',
                          type_='user')
+
     chats = Relationship(self_view='prompt_chats',
                          self_view_kwargs={'id': '<id>'},
                          related_view='chat_list',
@@ -90,21 +99,29 @@ def display_last_modified(chat):
         fstring = '%m/%d/%y'
     return chat.date_modified.strftime(fstring).lstrip('0')
 
-
-class ChatSchema(AssetSchema):
+class ChatSchema(Schema):
     class Meta:
         type_ = 'chat'
         self_view = 'chat_detail'
         self_view_kwargs = {'id': '<id>'}
         self_view_many = 'chat_list'
+        inflect = underscore_to_camel
 
-    messages = fields.Function(lambda chat: chat.prompt.messages + chat.messages)
+    id = fields.Integer(as_string=True, dump_only=True)
+    date_created = fields.DateTime(dump_only=True)
+    date_modified = fields.DateTime()
+
+    visible = fields.Bool()
+    active = fields.Bool()
+
+    owner_id = fields.Integer(as_string=True, load_only=True)
     owner = Relationship(self_view='chat_user',
                          self_view_kwargs={'id': '<id>'},
                          related_view='user_detail',
                          related_view_kwargs={'chat_id': '<id>'},
                          schema='UserSchema',
                          type_='user')
+
     prompt = Relationship(self_view='chat_prompt',
                           self_view_kwargs={'id': '<id>'},
                           related_view='prompt_detail',
